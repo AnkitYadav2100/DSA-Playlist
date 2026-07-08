6693e5463629ac69746e50fc700e1779246b9c68
Ankit Yadav <ay766681@gmail.com>
2026-07-08 23:41:27 +0530

    Time: 149 ms (89.74%), Space: 132.7 MB (9.03%) - LeetHub

--- 0204-count-primes/0204-count-primes.cpp
+++ 0204-count-primes/0204-count-primes.cpp
@@ -1,16 +1,5 @@
 class Solution {
 public:
-    // bool isPrime(int num){
-    //     if(num <=1 ) return false;
-    //     if(num == 2) return true;
-    //     if(num % 2 == 0) return false;
-    //     for(int i=3; i*i<=num; i+=2){
-    //         if(num%i==0){
-    //             return false;
-    //         }
-    //     }
-    //     return true;
-    // }
     int countPrimes(int n) {
         if(n <2) return 0;
         int count = 0;
@@ -18,7 +7,7 @@ public:
         temp[0] = temp[1] = 0;
         for(int i=2; i*i<n; i++){
             if(temp[i]){
-                for(int j=i*i; j<n; j+=i){
+                for(int j=2*i; j<n; j+=i){
                     temp[j] = 0;
                 }
             }
